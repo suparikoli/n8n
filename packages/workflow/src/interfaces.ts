9567f67023795a6e0c646e33ea4f22d286709fe4
Bernhard Wittmann <bernhard.wittmann@n8n.io>
2026-07-16 17:06:30 +0200

    refactor(core)!: Migrate node defaults.color to iconColor (#34180)

--- packages/workflow/src/interfaces.ts
+++ packages/workflow/src/interfaces.ts
@@ -2854,10 +2854,6 @@ export interface INodeOutputConfiguration {
 export type ExpressionString = `={{${string}}}`;
 
 export type NodeDefaults = Partial<{
-	/**
-	 * @deprecated Use {@link INodeTypeBaseDescription.iconColor|iconColor} instead. `iconColor` supports dark mode and uses preset colors from n8n's design system.
-	 */
-	color: string;
 	name: string;
 }>;
 
